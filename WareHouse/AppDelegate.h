aa8d26b824e3ff6dd085a63d679e2f106fa4d175
Lan David <David@10-100-231-104-eduroam.student.utas.edu.au>
2017-05-12 15:26:50 +1000

    Initial Commit

---
+++ WareHouse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WareHouse
+//
+//  Created by David Lan on 12/5/17.
+//  Copyright © 2017 David Lan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WareHouse/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WareHouse
+//
+//  Created by David Lan on 12/5/17.
+//  Copyright © 2017 David Lan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
