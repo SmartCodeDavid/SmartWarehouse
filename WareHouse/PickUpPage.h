15deff70a54a6c8277e0575c428085dc6831ff15
David Lan <David@MacBook-Pro-3.local>
2018-03-13 17:40:04 +1100

    first commit

---
+++ WareHouse/PickUpPage.h
@@ -0,0 +1,13 @@
+//
+//  PickUpPage.h
+//  WareHouse
+//
+//  Created by Gerry Gao on 18/6/17.
+//  Copyright © 2017 David Lan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface PickUpPage : ViewController
+
+@end
